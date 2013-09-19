52ce167f9d6287fbd97fbe321df10a7f458c2795
Filipe <goulartfs@gmail.com>
2013-09-19 00:14:42 -0300

    Início de layout

---
+++ apps/frontend/modules/main/templates/_form.php
@@ -0,0 +1,14 @@
+<form action="<?php print url_for('@homepage') ?>" method="post">
+    <?php print $form['url']->renderError() ?>
+    <div class="display-fields">
+        <?php print $form['url'] ?>
+        <?php print $form['_csrf_token'] ?>
+        <button class="" type="submit"><img src="/images/bt_encurtar.png" /></button>
+        <p>
+            <small>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+                Ut pretium, eros vel auctor dictum.
+            </small>
+        </p>
+    </div>
+</form>
