8052f4d4da9b5818fdeff1cafcb0e1630d07208c
Filipe <goulartfs@gmail.com>
2013-10-10 01:41:54 -0300

    Workaround de bug em carregamento de sidebar

--- apps/frontend/modules/commom/templates/_sidebar.php
+++ apps/frontend/modules/commom/templates/_sidebar.php
@@ -1,2 +1,9 @@
 <?php
-include_partial('commom/' . $sf_user->getAttribute('profile', 'publisher'));
+if ($sf_user->getAttribute() == 'publisher') {
+    include_partial('commom/publisher');
+} else if ($sf_user->getAttribute() == 'advertiser') {
+    include_partial('commom/advertiser');
+} else {
+    include_partial('commom/publisher');
+    $sf_user->setAttribute('profile', 'publisher');
+}
