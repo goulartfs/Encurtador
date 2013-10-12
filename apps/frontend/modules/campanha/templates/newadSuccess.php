e89eaf9a69d77d2943fe6af678a77fd84d577cb7
goulartfs <goulartfs@gmail.com>
2013-10-12 01:06:28 -0300

    Info area para notice e error flashes

--- apps/frontend/modules/campanha/templates/newadSuccess.php
+++ apps/frontend/modules/campanha/templates/newadSuccess.php
@@ -10,18 +10,7 @@
         <form class="form-horizontal" action="<?php print url_for('@campanha_new') ?>" method="post">
             <fieldset>
                 <legend>Nova Campanha</legend>
-                <?php if ($sf_user->hasFlash('error')): ?>
-                    <div class="alert alert-error">
-                        <button type="button" class="close" data-dismiss="alert">×</button>
-                        <div><?php echo $sf_user->getFlash('error') ?></div>
-                    </div>
-                <?php endif; ?>
-                <?php if ($sf_user->hasFlash('notice')): ?>
-                    <div class="alert alert-success">
-                        <button type="button" class="close" data-dismiss="alert">×</button>
-                        <div><?php echo $sf_user->getFlash('notice') ?></div>
-                    </div>
-                <?php endif; ?>
+                <?php include_partial('commom/infoArea'); ?>
                 <?php include_partial('commom/form', array('form' => $form)) ?>
                 <div class="control-group">
                     <div class="controls">
