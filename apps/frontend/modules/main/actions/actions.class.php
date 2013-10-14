18c92ac95953119521726f8919149e33fbff579d
goulartfs <goulartfs@gmail.com>
2013-10-14 10:10:52 -0300

    Encurtamento de link na home quando usuário não está logado

--- apps/frontend/modules/main/actions/actions.class.php
+++ apps/frontend/modules/main/actions/actions.class.php
@@ -15,11 +15,6 @@ class mainActions extends sfActions {
      *
      * @param sfRequest $request A request object
      */
-    public function preExecute() {
-        parent::preExecute();
-        $this->getResponse()->setCookie('testcookie', 'test');
-    }
-
     public function executeIndex(sfWebRequest $request) {
         if ($this->getUser()->isAuthenticated()) {
             if (!$this->getUser()->hasAttribute('profile_preference')) {
@@ -36,18 +31,26 @@ class mainActions extends sfActions {
         }
 
         $this->form = new EncurtadorForm();
-        $encurtados = $this->getRequest()->getCookie('encurtados');
-
-        $url_ids = str_replace('-', ',', $encurtados);
         $this->urls = null;
+
+        if ($request->getMethod() == 'POST') {
+
+            $this->form->bind($request->getParameter('encurtador'));
+            if ($this->form->isValid()) {
+
+                $this->url = $this->form->process();
+
+                if ($this->getUser()->isAuthenticated()) {
+                    $this->redirect('profile/links');
+                }
+            }
+        }
 
         $this->setLayout('layout');
     }
 
     public function executeResolve(sfWebRequest $request) {
         $this->setLayout('link');
-
-        $has_cookie = (boolean) ($this->getRequest()->getCookie('testcookie'));
 
         $this->forward404If(!$request->getParameter('url_id'));
         $url = Doctrine::getTable('Url')->findOneByShortUrl($request->getParameter('url_id'));
@@ -56,32 +59,20 @@ class mainActions extends sfActions {
         $this->ad = Doctrine::getTable('Campanha')->createQuery('c')
                         ->where('c.is_active = 1')
                         ->addWhere('c.is_payment_processed = 1')
-                        ->addWhere('c.status_transacao_id = 6')
                         ->addWhere('c.is_finished <> 1')
                         ->orderBy('RAND()')
                         ->execute()->getFirst();
 
-        $time = sfConfig::get('user_time_block') * 60 * 60;
-        if (!$this->getRequest()->getCookie($request->getParameter('url_id')) && $has_cookie) {
-            $this->getResponse()->setCookie($request->getParameter('url_id'), md5(uniqid()), time() + $time);
-
-            $url_controle = new UrlControle();
-            $url_controle->setIpuser($_SERVER['REMOTE_ADDR']);
-            $url_controle->setUrlId($url->getId());
-            $url_controle->save();
-        }
+        $url_controle = new UrlControle();
+        $url_controle->setIpuser($_SERVER['REMOTE_ADDR']);
+        $url_controle->setUrlId($url->getId());
+        $url_controle->save();
 
         if ($this->ad) {
             $ad_controle = new CampanhaControle();
             $ad_controle->setIpViewer($_SERVER['REMOTE_ADDR']);
             $ad_controle->setCampanhaId($this->ad->getId());
             $ad_controle->save();
-        }
-
-        if ($this->ad && $this->ad->getTotal() >= $this->ad->getOrcamento()->getQuantidade()) {
-            $this->ad->setIsFinished(1);
-            $this->ad->setEndDate(date('Y-m-d h:i:s'));
-            $this->ad->save();
         }
 
         $this->url = $url;
