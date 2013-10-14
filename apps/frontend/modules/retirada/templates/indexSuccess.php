059756bc5e14098ee372bc365727b5f34fd92df2
goulartfs <goulartfs@gmail.com>
2013-10-14 16:38:47 -0300

    Texto revisado

--- apps/frontend/modules/retirada/templates/indexSuccess.php
+++ apps/frontend/modules/retirada/templates/indexSuccess.php
@@ -1,29 +1,20 @@
 <h1 class="head_page">Resgatar seus ganhos</h1>
 <p>
-    Se os seus rendimentos chegarem a um total de R$ 8,00 ou mais, você poderá solicitar a retirada a qualquer momento, seus ganhos serão pagos em até 3 dias úteis, após o pedido de retirada. Verifique se o seu usuario Paypal, ou dados bancarios  estão corretos antes de confirmar, caso contrário, não nos responsabilizamos por erros por parte do usuário publisher.
+    Se os seus rendimentos chegarem a um total de R$ 8,00 ou mais, você poderá solicitar a retirada a qualquer momento, seus ganhos serão pagos em até 3 dias úteis, após o pedido de retirada. Verifique se o seu usuario Paypal, ou dados bancarios estão corretos antes de confirmar, caso contrário, não nos responsabilizamos por erros por parte do usuário publisher.
 </p>
 <p>
-    Atingindo o valor minimo de R$ 8,00 , aparecerá logo abaixo um menu com as opções de retirada, com isto basta escolher à qualquer momento  a que opção que melhor se encaixe para você.
+    Atingindo o valor minimo de R$ 8,00 , aparecerá logo abaixo um menu com as opções de retirada, com isto basta escolher à qualquer momento a  opção que melhor se encaixe para você.
 </p>
 <div class="row">
     <div class="span12">
-        <!--<div class="well">-->
         <div class="row">
             <div class="span4">
-<!--                <h3>Todo ganho disponível</h3>
-                <div class="">
-                    <h4>
-                        R$ <?php print $ganhos ?>
-                    </h4>
-                    <h5>TOTAL</h5>
-                </div>-->
             </div>
             <div class="span4">
             </div>
             <div class="span4 text-right">
             </div>
         </div>
-        <!--</div>-->
         <hr>
     </div>
     <?php if ($sf_user->hasFlash('error')): ?>
