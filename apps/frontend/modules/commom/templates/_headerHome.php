486de5530cf6320d6034638a672d068f9eac09cc
goulartfs <goulartfs@gmail.com>
2013-10-14 16:43:21 -0300

    Texto revisado

--- apps/frontend/modules/commom/templates/_headerHome.php
+++ apps/frontend/modules/commom/templates/_headerHome.php
@@ -13,7 +13,7 @@
             <div class="row pos-pq">
                 <p>
                     <small>
-                        Porque Pagamos R$ 4 reais para cada 1000 Views, valor bem superios aos  demais encurtadores.Toda a publicidade é estritamente familiar, segura e sem popups. Servidores de anti-vírus estarão escaneando os anúncios 24/7.Pagamentos minimos, 8 reais. Pagaremos todos usuarios através do Paypal, Pagseguro ou depósito em conta.
+                        Porque Pagamos R$ 4 reais para cada 1000 Views, valor bem superior aos demais encurtadores. Toda a publicidade é estritamente familiar, segura e sem popups. Servidores de antivírus estarão escaneando os anúncios 24/7. Pagamento mínimo R$ 8,00 reais. Pagaremos todos os usuários através do Paypal, Pagseguro ou depósito em conta.
                     </small>
                 </p>
             </div>
