eefad9a502180308c7bcdd4a3a6cd18e1b98e93f
goulartfs <goulartfs@gmail.com>
2013-10-15 13:31:18 -0300

    Atualização de texto

--- apps/frontend/modules/retirada/templates/infoSuccess.php
+++ apps/frontend/modules/retirada/templates/infoSuccess.php
@@ -1,2 +1,2 @@
 <h2>Resgatar seus ganhos</h2>
-<p>Seus ganhos serão em até 3 dias uteis, se os seus rendimentos chegaram a um total de R$ 8,00 ou mais. Atingindo esse valor minimo você pode pedir a retirada a qualquer momento. Verifique se o seu usuario Paypal, ou dados bancarios  estão corretos antes de confirmar, caso contrário, você não pode ser pago.</p>
+<p>Se os seus rendimentos chegarem a um total de R$ 8,00 ou mais, você poderá solicitar a retirada a qualquer momento, seus ganhos serão pagos em até 3 dias úteis, após o pedido de retirada. Verifique se o seu usuário Paypal, ou dados bancários estão corretos antes de confirmar, caso contrário, não nos responsabilizamos por erros por parte do usuário Publisher.</p>
