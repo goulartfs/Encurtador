c6937ee34248a40775faeb586f4491be0edf8bdb
Hugo Dutra <hugo.dutra@uniriotec.br>
2014-03-06 15:03:17 -0300

    pequenas modificacoes na home

--- web/js/hugo_home.js
+++ web/js/hugo_home.js
@@ -1,6 +1,7 @@
 $('.cadastro').click(function(){
 	$('#form').removeClass('esconder').hasClass('esconder');
 	$('#form-login').addClass('esconder');
+	$('.cadastro-login').addClass('border');
 	$('#form-cadastro').removeClass('esconder');
 	$('.cadastro img').css('transform', 'rotate(90deg)');
 	$('.login img').css('transform', 'rotate(0deg)');
@@ -8,6 +9,7 @@ $('.cadastro').click(function(){
 
 $('.login').click(function() {
 	$('#form').removeClass('esconder').hasClass('esconder');
+	$('.cadastro-login').addClass('border');
 	$('#form-login').removeClass('esconder');
 	$('#form-cadastro').addClass('esconder');
 	$('.cadastro img').css('transform', 'rotate(0deg)');
