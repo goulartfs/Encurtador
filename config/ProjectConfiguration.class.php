cc742dd92556e10b942d25121cc185b0f0334983
goulartfs <goulartfs@gmail.com>
2014-03-27 12:09:42 -0300

    Configuração de Webdir em prod

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -3,12 +3,18 @@
 require_once dirname(__FILE__) . '/../lib/symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
-class ProjectConfiguration extends sfProjectConfiguration {
+class ProjectConfiguration extends sfProjectConfiguration
+{
 
-    public function setup() {
+    public function setup()
+    {
 
         date_default_timezone_set('America/Sao_Paulo');
         define("URL_BASE", "http://" . $_SERVER['HTTP_HOST']);
+
+
+        # Descomentar em Prod.
+        #$this->setWebDir($this->getRootDir() . "/../public_html");
 
         sfConfig::set('bancos', array(
             'Bradesco' => 'Bradesco',
@@ -18,12 +24,12 @@ class ProjectConfiguration extends sfProjectConfiguration {
             'Caixa Econômica' => 'Caixa Econômica',
             'Banco do Brasil' => 'Banco do Brasil'
         ));
-        
-        sfConfig::set('user_time_block', (1/60));
+
+        sfConfig::set('user_time_block', (1 / 60));
 
         $this->enablePlugins('sfDoctrinePlugin');
         $this->enablePlugins('sfDoctrineGuardPlugin');
-      $this->enablePlugins('sfThumbnailPlugin');
-  }
+        $this->enablePlugins('sfThumbnailPlugin');
+    }
 
 }
